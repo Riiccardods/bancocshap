b408583a23b64b3190be41b1ab0e31f103658cd6
riicardods <riicardoljn@icloud.com>
2023-10-05 18:56:00 -0300

    Adicionar arquivos de projeto.

---
+++ bancocshap/Models/ContaBancaria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bancocshap.Models
+{
+    internal class ContaBancaria
+    {
+        public Titular Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(Titular titular, double saldoAbertura)
+        {
+            Titular = titular;
+            Saldo = saldoAbertura;
+        }
+
+
+
+    }
+}
---
+++ bancocshap/Program.cs
@@ -0,0 +1,25 @@
+﻿using bancocshap.Models;
+
+Console.WriteLine("****************************************");
+Console.WriteLine("************ Banco Csharp **************");
+Console.WriteLine("****************************************");
+Console.WriteLine();
+
+var endereco = new Endereco
+{
+    Cep = "2890030",
+    Rua = "rua xyz",
+    Numero = 53
+
+
+
+};
+
+var titular01 = new Titular("jose","12345","219997788");
+titular01.Endereco = endereco;
+
+
+
+var conta01 = new ContaBancaria(titular01, 100.0);
+
+Console.WriteLine(titular01.Telefone);
